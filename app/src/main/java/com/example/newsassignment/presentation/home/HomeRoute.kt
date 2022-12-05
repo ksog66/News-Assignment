cf40d74eb84272e3fe7086918674bd881369328c
Karan Sharma <ksog66@gmail.com>
2022-12-05 13:41:41 +0530

    Fix error screen ui

--- app/src/main/java/com/example/newsassignment/presentation/home/HomeRoute.kt
+++ app/src/main/java/com/example/newsassignment/presentation/home/HomeRoute.kt
@@ -2,6 +2,8 @@ package com.example.newsassignment.presentation.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -61,17 +64,17 @@ private fun ErrorScreen(modifier: Modifier = Modifier, errorMessage: String) {
         val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.error))
         LottieAnimation(
             modifier = Modifier
-                .weight(4f)
+                .fillMaxWidth()
+                .height(600.dp)
                 .align(Alignment.CenterHorizontally),
             composition = composition,
             iterations = LottieConstants.IterateForever
         )
 
         Text(
-            modifier = Modifier.weight(1f),
             text = errorMessage,
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onError
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.error
         )
     }
 }
